1571b26b8201b2590eb31bcb8cfbfc1cc9c15650
Akhil Roshan <akhilroshan111@gmail.com>
2025-03-08 23:59:38 +0530

    banner and haeader minor  modified

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Header.css';
 import { VscAccount } from "react-icons/vsc";
-import SearchBar from '../../assets/SearchBar';
+
 
 
 function Header() {
   return (
     <div className='ParentDiv'>
       <div className="ChildDiv">
-        <h1 className="logo">ABC STORE</h1>
+        <h1 className="logo">MENDI FANCY</h1>
         <div className="loginPage">
           <span>Login</span>
           <VscAccount id="profile-icon" />
